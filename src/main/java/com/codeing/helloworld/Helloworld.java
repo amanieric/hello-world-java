83f5a1fcfb16aebcf1fa93e854e0694af1754d94
YZO23931 <YZO23931@open-groupe.com>
2023-12-06 07:41:08 +0100

    initial commit -pom.xml

---
+++ src/main/java/com/codeing/helloworld/Helloworld.java
@@ -0,0 +1,10 @@
+package com.codeing.helloworld;
+
+import static java.lang.System.*;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        out.println("Hello world");
+    }
+}
